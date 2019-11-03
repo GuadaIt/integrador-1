e3d5edfb18a7dc4f968c949d97c14df1f362927c
GuadaIt <guada.iturralde@gmail.com>
2019-11-03 12:19:00 -0300

    arreglo algo y rompo otra cosa :')

--- integrador1.js
+++ integrador1.js
@@ -9,7 +9,7 @@ let unidadesProducto1 = 0;
 let unidadesProducto2 = 0;
 let modoDePago;
 let cantidadDeCuotas = 0; 
-let totalEnCuotas = "";
+let totalEnCuotas = 0;
 const respuestaAfirmativa = "Si";
 const respuestaNegativa = "No";
 
@@ -28,6 +28,8 @@ respuestaProducto2 == respuestaAfirmativa && (unidadesProducto2 = Number(prompt(
 let subtotal1 = precioProducto1 * unidadesProducto1;
 let subtotal2 = precioProducto2 * unidadesProducto2;
 let total = subtotal1 + subtotal2;
+let totalSinCuotas;
+totalEnCuotas = total / cantidadDeCuotas;
 
 alert(`El detalle de tu compra es:
 - ${producto1}: ${unidadesProducto1} x $${precioProducto1}
@@ -49,14 +51,14 @@ modoDePago == respuestaAfirmativa && (cantidadDeCuotas = Number(prompt("¿En cu�
   A abonar en ${cantidadDeCuotas} cuotas de $${totalEnCuotas} 
 `);
 
-(modoDePago == respuestaNegativa || modoDePago == "") && alert(`El detalle de tu compra es:
+(modoDePago == respuestaNegativa || modoDePago == "") && (totalSinCuotas = alert(`El detalle de tu compra es:
 - ${producto1}: ${unidadesProducto1} x $${precioProducto1}
   Subtotal: $${subtotal1}
 - ${producto2}: ${unidadesProducto2} x $${precioProducto2}
   Subtotal: $${subtotal2}
  
   Total: $${total}
-`)
+`));
 
 totalEnCuotas = total / cantidadDeCuotas;
 let valorDescuento = (10 * total) / 100;
@@ -90,9 +92,7 @@ ingresaCodigo == códigoDeDescuento && alert(`El detalle de tu compra es:
   A abonar en ${cantidadDeCuotas} cuotas de $${totalEnCuotas} 
 `)
 
-ingresaCodigo !== códigoDeDescuento && alert("Por favor, ingresa un código válido");
-
-//faltaria que aparezca el prompt despues del alerta "por favor ingresa un codigo valido"
+ingresaCodigo !== códigoDeDescuento && (ingresaCodigo = prompt("Por favor, ingresa un código válido"));
 
 ingresaCodigo == códigoDeDescuento && alert(`El detalle de tu compra es:
 - ${producto1}: ${unidadesProducto1} x $${precioProducto1}
@@ -106,5 +106,7 @@ ingresaCodigo == códigoDeDescuento && alert(`El detalle de tu compra es:
   Total final: $${totalFinal} 
   A abonar en ${cantidadDeCuotas} cuotas de $${totalEnCuotas} 
 `)
+
+//faltaría solucionar que aparezca bien el detalle de la compra si ambos el descuento y las cuotas son validas, o si solo una de ellas lo es
 
 alert(`¡Gracias por tomarte una Dosis con nosotros!`)
